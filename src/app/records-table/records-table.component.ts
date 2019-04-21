113f4956c9d8c644b2428d69d5681be2744a81cc
Bathenape <jordan.leahey@gmail.com>
2019-04-21 18:15:53 -0400

    changed api call structure

--- src/app/records-table/records-table.component.ts
+++ src/app/records-table/records-table.component.ts
@@ -58,10 +58,8 @@ export class RecordsTableComponent implements OnInit {
           console.log(data);
           this.data = data;
         }, 2000);
-      }
-    );
-    var i = 0;
-    this.data.array.forEach(element => {
+        var i = 0;
+      this.data.array.forEach(element => {
       this.rowdata[i] = [
       {PID8: element['message']['HL7']['source']["ORU_R01"]["ORU_R01-PATIENT_RESULT"]["ORU_R01-ORDER_OBSERVATION"]["ORU_R01-PATIENT"]["PID"]["PID-8"], 
       XAD6: element['message']['HL7']['source']["ORU_R01"]["ORU_R01-PATIENT_RESULT"]["ORU_R01-ORDER_OBSERVATION"]["ORU_R01-PATIENT"]["PID"]["PID-11"]["XAD-6"],
@@ -69,6 +67,8 @@ export class RecordsTableComponent implements OnInit {
       CE5: element['message']['HL7']['source']["ORU_R01"]["ORU_R01-PATIENT_RESULT"]["ORU_R01-ORDER_OBSERVATION"]["ORU_R01-OBSERVATION"]["OBX"]["OBX-5"]["CE-5"]}]
       i++;
     });
+      }
+    );
     /* this.mongoService.getLocalData().subscribe(
       data => {
         this.rowdata = [
